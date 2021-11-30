1ca64a659da9c5527fde8ec41768bb93d07199af
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-11-30 12:40:49 +0100

    Adding interface IPuzzleSolver

---
+++ Source/AdventOfCode.Common/IPuzzleSolver.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode.Common
+{
+	public interface IPuzzleSolver<T>
+	{
+		T Part1Solution { get; set; }
+
+		T Part2Solution { get; set; }
+		
+		void SolvePuzzle(string[] input);
+	}
+}
