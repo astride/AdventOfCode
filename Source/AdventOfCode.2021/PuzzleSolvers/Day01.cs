b4021c8814e2bc0fe11c22db80c3ceda65355afe
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-01 18:53:12 +0100

    Adding project AdventOfCode.2021 and PuzzleSolver and PuzzleInput for Day01

---
+++ Source/AdventOfCode.2021/PuzzleSolvers/Day01.cs
@@ -0,0 +1,76 @@
+﻿using AdventOfCode.Common;
+using System.Linq;
+
+namespace AdventOfCode.Y2021.PuzzleSolvers
+{
+	class Day01 : IPuzzleSolver<int>
+	{
+		public int Part1Solution { get; set; }
+		public int Part2Solution { get; set; }
+
+		public void SolvePuzzle(string[] rawInput)
+		{
+			var input = rawInput
+				.Where(entry => !string.IsNullOrWhiteSpace(entry))
+				.Select(entry => int.Parse(entry))
+				.ToArray();
+
+			Part1Solution = SolvePart1(input);
+			Part2Solution = SolvePart2(input);
+		}
+
+		private int SolvePart1(int[] sonarSweepReport)
+		{
+			int prevDepth = sonarSweepReport.First();
+			var increaseCount = 0;
+
+			foreach (var depth in sonarSweepReport.Skip(1))
+			{
+				if (depth > prevDepth)
+				{
+					increaseCount++;
+				}
+
+				prevDepth = depth;
+			}
+
+			return increaseCount;
+		}
+
+		private int SolvePart2(int[] sonarSweepReport)
+		{
+			int depthSum;
+			int prevDepthSum = sonarSweepReport.GetDepthSumForWindowAt(0);
+			var increaseCount = 0;
+
+			for (var i = 1; i <= sonarSweepReport.GetLastWindowIndex(); i++)
+			{
+				depthSum = sonarSweepReport.GetDepthSumForWindowAt(i);
+
+				if (depthSum > prevDepthSum)
+				{
+					increaseCount++;
+				}
+
+				prevDepthSum = depthSum;
+			}
+
+			return increaseCount;
+		}
+	}
+
+	static class Day01Part2Helper
+	{
+		private const int WindowSize = 3;
+
+		public static int GetLastWindowIndex(this int[] report)
+		{
+			return report.Length - WindowSize;
+		}
+
+		public static int GetDepthSumForWindowAt(this int[] report, int index)
+		{
+			return report.Skip(index).Take(WindowSize).Sum();
+		}
+	}
+}
