98ea5cf04ed0587577f6e9a0908704f83830ee90
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-06 08:33:55 +0100

    Preparations

---
+++ Source/AdventOfCode.2021/Day06/Solver.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Y2021
+{
+	public class Day06Solver : IPuzzleSolver
+	{
+		public string Part1Solution { get; set; }
+		public string Part2Solution { get; set; }
+
+		public void SolvePuzzle(string[] rawInput)
+		{
+			var input = rawInput;
+
+			Part1Solution = SolvePart1(input).ToString();
+		}
+
+		private static int SolvePart1(string[] input)
+		{
+			//TODO
+			return -1;
+		}
+	}
+}
