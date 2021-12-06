e095965a0946a06b8197dd9769cf34f9c8ef33b0
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-06 19:50:29 +0100

    Part1

--- Source/AdventOfCode.2021/Day06/Solver.cs
+++ Source/AdventOfCode.2021/Day06/Solver.cs
@@ -12,15 +12,70 @@ namespace AdventOfCode.Y2021
 
 		public void SolvePuzzle(string[] rawInput)
 		{
-			var input = rawInput;
+			var input = rawInput
+				.Where(entry => !string.IsNullOrWhiteSpace(entry))
+				.Single()
+				.Split(',')
+				.Where(entry => !string.IsNullOrWhiteSpace(entry))
+				.Select(entry => int.Parse(entry));
 
 			Part1Solution = SolvePart1(input).ToString();
 		}
 
-		private static int SolvePart1(string[] input)
+		private const int Days = 80;
+
+		private static int SolvePart1(IEnumerable<int> timerValues)
 		{
-			//TODO
-			return -1;
+			var shoal = timerValues.ToList();
+
+			foreach (var _ in Enumerable.Range(0, Days))
+			{
+				shoal.SimulatePopulationChange();
+			}
+
+			return shoal.Count();
+		}
+	}
+
+	public static class Day06Helpers
+	{
+		private const int TimerValueInit = 8;
+		private const int TimerValueAboutToSpawn = 0;
+		private const int TimerValueAfterSpawning = 6;
+
+		public static void SimulatePopulationChange(this List<int> population)
+		{
+			var spawnCount = population.Count(entity => entity.IsReadyToSpawn());
+
+			population.UpdateInternalTimers();
+			population.Spawn(spawnCount);
+		}
+
+		private static bool IsReadyToSpawn(this int maternalTimer)
+		{
+			return maternalTimer == TimerValueAboutToSpawn;
+		}
+
+		private static void UpdateInternalTimers(this List<int> timers)
+		{
+			for (var i = 0; i < timers.Count(); i++)
+			{
+				timers[i] = timers[i].GetNextValue();
+			}
+		}
+
+		private static int GetNextValue(this int value)
+		{
+			return value == TimerValueAboutToSpawn
+				? TimerValueAfterSpawning
+				: value - 1;
+		}
+
+		private static void Spawn(this List<int> population, int count)
+		{
+			if (count == 0) return;
+
+			population.AddRange(Enumerable.Repeat(TimerValueInit, count));
 		}
 	}
 }
