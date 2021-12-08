d49a11d17ab5ff3a586618b8f6665adcf8ebc9c1
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-08 09:56:46 +0100

    Part 1

--- Source/AdventOfCode.2021/Day08/Solver.cs
+++ Source/AdventOfCode.2021/Day08/Solver.cs
@@ -12,20 +12,56 @@ namespace AdventOfCode.Y2021
 
 		public void SolvePuzzle(string[] rawInput)
 		{
-			var input = rawInput
+			List<(string[] InputValues, string[] OutputValues)> input;
+
+			if (rawInput.First().Last() == '|')
+            {
+				//We have example data
+				var inputValues = rawInput.Where(entry => entry.Last() == '|').ToList();
+				var outputValues = rawInput.Where(entry => entry.Last() != '|').ToList();
+
+				input = new List<(string[] InputValues, string[] OutputValues)>();
+
+				foreach (var i in Enumerable.Range(0, inputValues.Count()))
+                {
+					input.Add((inputValues[i].Split(' '), outputValues[i].Split(' ')));
+                }
+            }
+			else
+            {
+				//We have real data
+				input = rawInput
 				.Where(entry => !string.IsNullOrWhiteSpace(entry))
-				.Single()
-				.Split(',')
-				.Select(entry => int.Parse(entry))
-				.ToArray();
+				.Select(entry => entry.Split('|'))
+				.Select(entry =>
+					(entry[0]
+						.Split(' ')
+						.Where(inputValue => !string.IsNullOrWhiteSpace(inputValue))
+						.ToArray(),
+					entry[1]
+						.Split(' ')
+						.Where(outputValue => !string.IsNullOrWhiteSpace(outputValue))
+						.ToArray()))
+				.ToList();
+			}
 
 			Part1Solution = SolvePart1(input).ToString();
 		}
 
-		private static int SolvePart1(int[] input)
+		private const int CharCountDigit1 = 2;
+		private const int CharCountDigit4 = 4;
+		private const int CharCountDigit7 = 3;
+		private const int CharCountDigit8 = 7;
+
+		private readonly static int[] SimpleDigits = new int[] { CharCountDigit1, CharCountDigit4, CharCountDigit7, CharCountDigit8 };
+
+		private static int SolvePart1(List<(string[] InputValues, string[] OutputValues)> input)
 		{
-			//TODO
-			return -1;
+			var simpleDigitCount = input
+				.SelectMany(input => input.OutputValues)
+				.Count(outputValue => SimpleDigits.Contains(outputValue.Count()));
+
+			return simpleDigitCount;
 		}
 	}
 
