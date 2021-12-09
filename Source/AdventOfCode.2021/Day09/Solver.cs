a38b26fe26410a66a28c9cc52990e2e3e526b60a
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-09 07:35:45 +0100

    Part 1

--- Source/AdventOfCode.2021/Day09/Solver.cs
+++ Source/AdventOfCode.2021/Day09/Solver.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace AdventOfCode.Y2021
 {
-	public class Day08Solver : IPuzzleSolver
+	public class Day09Solver : IPuzzleSolver
 	{
 		public string Part1Solution { get; set; }
 		public string Part2Solution { get; set; }
@@ -14,18 +14,37 @@ namespace AdventOfCode.Y2021
 		{
 			var input = rawInput
 				.Where(entry => !string.IsNullOrWhiteSpace(entry))
-				.Single()
-				.Split(',')
-				.Select(entry => int.Parse(entry))
 				.ToArray();
 
 			Part1Solution = SolvePart1(input).ToString();
 		}
 
-		private static int SolvePart1(int[] input)
+		private static int SolvePart1(string[] heightMap)
 		{
-			//TODO
-			return -1;
+			var caveIndexMin = 0;
+			var caveIndexMax = heightMap.Length - 1;
+			var locationIndexMin = 0;
+			var locationIndexMax = heightMap.First().Length - 1;
+
+			var lowPoints = new List<int>();
+
+			foreach (var caveIndex in Enumerable.Range(caveIndexMin, caveIndexMax + 1))
+			{
+				foreach (var locationIndex in Enumerable.Range(locationIndexMin, locationIndexMax + 1))
+				{
+					if ((caveIndex == caveIndexMin || (heightMap[caveIndex])[locationIndex] < (heightMap[caveIndex - 1])[locationIndex]) &&
+						(caveIndex == caveIndexMax || (heightMap[caveIndex][locationIndex]) < (heightMap[caveIndex + 1])[locationIndex]) &&
+						(locationIndex == locationIndexMin || (heightMap[caveIndex][locationIndex]) < (heightMap[caveIndex])[locationIndex - 1]) &&
+						(locationIndex == locationIndexMax || (heightMap[caveIndex][locationIndex]) < (heightMap[caveIndex])[locationIndex + 1]))
+					{
+						lowPoints.Add(int.Parse((heightMap[caveIndex])[locationIndex].ToString()));
+					}
+				}
+			}
+
+			var riskLevelSum = lowPoints.Count() + lowPoints.Sum();
+
+			return riskLevelSum;
 		}
 	}
 }
