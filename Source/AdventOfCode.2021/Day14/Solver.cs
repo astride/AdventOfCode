8dce73f52cab8517947c49d208bdded9bc289741
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-14 06:59:35 +0100

    Part 1

--- Source/AdventOfCode.2021/Day14/Solver.cs
+++ Source/AdventOfCode.2021/Day14/Solver.cs
@@ -12,19 +12,47 @@ namespace AdventOfCode.Y2021
 
 		public void SolvePuzzle(string[] rawInput)
 		{
-			var input = rawInput;
+			var polymerTemplate = rawInput.First();
 
-			Part1Solution = SolvePart1(input).ToString();
+			IDictionary<string, string> resultForPair = rawInput
+				.Skip(2)
+				.Select(entry => entry.Split(' '))
+				.ToDictionary(
+					rule => rule.First(), 
+					rule => rule.First().First() + rule.Last());
+
+			Part1Solution = SolvePart1(polymerTemplate, resultForPair).ToString();
 		}
 
-		private static int SolvePart1(string[] input)
+		private static int SolvePart1(string polymerTemplate, IDictionary<string, string> resultForPair)
 		{
-			return input.Count();
+			foreach (var _ in Enumerable.Range(0, 10))
+			{
+				polymerTemplate = polymerTemplate.GetNextPolymer(resultForPair);
+			}
+
+			var countPerElement = polymerTemplate
+				.GroupBy(ch => ch)
+				.Select(gr => gr.Count());
+
+			return countPerElement.Max() - countPerElement.Min();
 		}
 	}
 
 	public static class Day14Helpers
 	{
+		public static string GetNextPolymer(this string polymerTemplate, IDictionary<string, string> resultForPair)
+		{
+			var results = new List<string>();
 
+			foreach (var i in Enumerable.Range(0, polymerTemplate.Length - 1))
+			{
+				results.Add(resultForPair[polymerTemplate.Substring(i, 2)]);
+			}
+
+			results.Add(polymerTemplate.Last().ToString());
+
+			return string.Concat(results);
+		}
 	}
 }
