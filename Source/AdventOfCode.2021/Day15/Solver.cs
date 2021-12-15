8ff0a745fca5210b3897e26af4d0a78ab06ddcdb
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-15 16:21:47 +0100

    Part 1

--- Source/AdventOfCode.2021/Day15/Solver.cs
+++ Source/AdventOfCode.2021/Day15/Solver.cs
@@ -12,6 +12,7 @@ namespace AdventOfCode.Y2021
 
 		public void SolvePuzzle(string[] rawInput)
 		{
+			//Info: The shape of the cavern resembles a square
 			var map = rawInput
 				.Where(entry => !string.IsNullOrWhiteSpace(entry))
 				.ToArray();
@@ -19,14 +20,65 @@ namespace AdventOfCode.Y2021
 			Part1Solution = SolvePart1(map).ToString();
 		}
 
-		private static int SolvePart1(string[] riskLevelMap)
+		private static double SolvePart1(string[] map)
 		{
-			return -1;
+			var riskLevelMap = map.AsRiskLevelMap();
+			var lowestTotalRisk = riskLevelMap.GetLowestTotalRiskOfSouthEastFacingPath();
+
+			return lowestTotalRisk;
 		}
 	}
 
 	public static class Day15Helpers
 	{
+		private static double[,] LowestTotalRiskMap;
 
+		public static double[,] AsRiskLevelMap(this string[] squaredMap)
+		{
+			var mapSize = squaredMap.Length;
+
+			var map = new double[mapSize, mapSize];
+
+			foreach (var y in Enumerable.Range(0, mapSize))
+			{
+				foreach (var x in Enumerable.Range(0, mapSize))
+				{
+					map[x, y] = int.Parse((squaredMap[y])[x].ToString());
+				}
+			}
+
+			return map;
+		}
+
+		public static double GetLowestTotalRiskOfSouthEastFacingPath(this double[,] squaredMap)
+		{
+			var mapSize = squaredMap.GetLength(0);
+
+			LowestTotalRiskMap = new double[mapSize, mapSize];
+
+			foreach (var i in Enumerable.Range(1, mapSize - 1))
+			{
+				//calculate top--bottom along y axis
+				foreach (var y in Enumerable.Range(0, i))
+				{
+					LowestTotalRiskMap[i, y] = y == 0
+						? LowestTotalRiskMap[i - 1, y] + squaredMap[i, y]
+						: Math.Min(LowestTotalRiskMap[i - 1, y], LowestTotalRiskMap[i, y - 1]) + squaredMap[i, y];
+				}
+
+				//calculate left--right along x axis
+				foreach (var x in Enumerable.Range(0, i))
+				{
+					LowestTotalRiskMap[x, i] = x == 0
+						? LowestTotalRiskMap[x, i - 1] + squaredMap[x, i]
+						: Math.Min(LowestTotalRiskMap[x, i - 1], LowestTotalRiskMap[x - 1, i]) + squaredMap[x, i];
+				}
+
+				//calculate corner
+				LowestTotalRiskMap[i, i] = Math.Min(LowestTotalRiskMap[i - 1, i], LowestTotalRiskMap[i, i - 1]) + squaredMap[i, i];
+			}
+
+			return LowestTotalRiskMap[mapSize - 1, mapSize - 1];
+		}
 	}
 }
