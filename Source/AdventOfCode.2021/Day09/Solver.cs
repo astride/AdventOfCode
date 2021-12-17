59f4abf58ea9d33d46d19968c4e384a594ce6872
astride <6726401+astride@users.noreply.github.com>
2021-12-17 18:31:36 +0100

    [ 2021 - 09 ] Making simplifications (#13)
    * Simplifying the two GetBasinSize() methods
    
    * Moving classes from Day17 to Common
    
    * Using Coordinate class in Day9
    
    * Simplifying GetBasinSize() further

--- Source/AdventOfCode.2021/Day09/Solver.cs
+++ Source/AdventOfCode.2021/Day09/Solver.cs
@@ -1,5 +1,5 @@
 ﻿using AdventOfCode.Common;
-using System;
+using AdventOfCode.Common.Classes;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -33,9 +33,9 @@ namespace AdventOfCode.Y2021
 		private static int SolvePart2(string[] heightMap)
 		{
 			var basinMap = heightMap.GenerateFramedBoolMap();
-			
+
 			var basinOrigoCoordinates = heightMap.GetLowPointCoordinates()
-				.Select(coor => (coor.X + 1, coor.Y + 1));
+				.Select(coor => new Coordinate(coor.X + 1, coor.Y + 1));
 
 			var largestBasins = basinMap.GetLargestBasins(3, basinOrigoCoordinates);
 
@@ -45,31 +45,19 @@ namespace AdventOfCode.Y2021
 
 	public static class Day09Helpers
 	{
-		private static (int X, int Y) Above = (0, 1);
-		private static (int X, int Y) Below = (0, -1);
-		private static (int X, int Y) Left = (-1, 0);
-		private static (int X, int Y) Right = (1, 0);
+		private static readonly Coordinate Above = new Coordinate(0, 1);
+		private static readonly Coordinate Right = new Coordinate(1, 0);
+		private static readonly Coordinate Below = new Coordinate(0, -1);
+		private static readonly Coordinate Left	= new Coordinate(-1, 0);
 
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtOrigo = new List<(int X, int Y)> { Above, Below, Left, Right };
-		
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtTopRightEdge = new List<(int X, int Y)> { Above, Right };
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtBottomRightEdge = new List<(int X, int Y)> { Below, Right };
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtBottomLeftEdge = new List<(int X, int Y)> { Below, Left };
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtTopLeftEdge = new List<(int X, int Y)> { Above, Left };
-
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtCornerTop = NeighboringCoorsToCheckAtTopLeftEdge.Append(Right);
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtCornerRight = NeighboringCoorsToCheckAtTopRightEdge.Append(Below);
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtCornerBottom = NeighboringCoorsToCheckAtBottomRightEdge.Append(Left);
-		private static IEnumerable<(int X, int Y)> NeighboringCoorsToCheckAtCornerLeft = NeighboringCoorsToCheckAtBottomLeftEdge.Append(Above);
-
-		public static IEnumerable<(int X, int Y)> GetLowPointCoordinates(this string[] map)
+		public static IEnumerable<Coordinate> GetLowPointCoordinates(this string[] map)
 		{
 			var caveIndexMin = 0;
 			var caveIndexMax = map.Length - 1;
 			var locationIndexMin = 0;
 			var locationIndexMax = map.First().Length - 1;
 
-			var lowPointCoordinates = new List<(int X, int Y)>();
+			var lowPointCoordinates = new List<Coordinate>();
 
 			foreach (var caveIndex in Enumerable.Range(caveIndexMin, caveIndexMax + 1))
 			{
@@ -80,7 +68,7 @@ namespace AdventOfCode.Y2021
 						(locationIndex == locationIndexMin || (map[caveIndex][locationIndex]) < (map[caveIndex])[locationIndex - 1]) &&
 						(locationIndex == locationIndexMax || (map[caveIndex][locationIndex]) < (map[caveIndex])[locationIndex + 1]))
 					{
-						lowPointCoordinates.Add((locationIndex, caveIndex));
+						lowPointCoordinates.Add(new Coordinate(locationIndex, caveIndex));
 					}
 				}
 			}
@@ -109,7 +97,7 @@ namespace AdventOfCode.Y2021
 			return boolMap;
 		}
 
-		public static IEnumerable<int> GetLargestBasins(this bool[,] map, int basinCount, IEnumerable<(int X, int Y)> startingPoints)
+		public static IEnumerable<int> GetLargestBasins(this bool[,] map, int basinCount, IEnumerable<Coordinate> startingPoints)
 		{
 			var largestBasins = Enumerable.Repeat(0, basinCount).ToList();
 			int basinSize;
@@ -128,111 +116,34 @@ namespace AdventOfCode.Y2021
 			return largestBasins;
 		}
 
-		private static int GetBasinSize(this (int X, int Y) origo, bool[,] map)
+		private static int GetBasinSize(this Coordinate origo, bool[,] map)
 		{
-			return 1
-				+ NeighboringCoorsToCheckAtOrigo
-					.Where(coors => map[origo.X + coors.X, origo.Y + coors.Y])
-					.Select(coors => (origo.X + coors.X, origo.Y + coors.Y))
-					.GetBasinSize(map, origo);
+			return new List<Coordinate> { origo }.GetBasinSize(map, origo);
 		}
 
-		public static int GetBasinSize(this IEnumerable<(int X, int Y)> thisLevelCoors, bool[,] map, (int X, int Y) refCoor)
+		public static int GetBasinSize(this IEnumerable<Coordinate> thisLevelCoors, bool[,] map, Coordinate refCoor)
 		{
 			if (!thisLevelCoors.Any())
 			{
 				return 0;
 			}
 
-			var cornerTopCoor = thisLevelCoors.SingleOrDefault(coor => coor.X == refCoor.X && coor.Y > refCoor.Y);
-			var cornerRightCoor = thisLevelCoors.SingleOrDefault(coor => coor.X > refCoor.X && coor.Y == refCoor.Y);
-			var cornerBottomCoor = thisLevelCoors.SingleOrDefault(coor => coor.X == refCoor.X && coor.Y < refCoor.Y);
-			var cornerLeftCoor = thisLevelCoors.SingleOrDefault(coor => coor.X < refCoor.X && coor.Y == refCoor.Y);
+			var coorsToSearchFromByCoorShift = new Dictionary<Coordinate, IEnumerable<Coordinate>>
+			{
+				[Above] = thisLevelCoors.Where(coor => coor.Y >= refCoor.Y),
+				[Right] = thisLevelCoors.Where(coor => coor.X >= refCoor.X),
+				[Below] = thisLevelCoors.Where(coor => coor.Y <= refCoor.Y),
+				[Left] = thisLevelCoors.Where(coor => coor.X <= refCoor.X)
+			};
 
-			var topRightCoors = thisLevelCoors.Where(coor => coor.X > refCoor.X && coor.Y > refCoor.Y);
-			var bottomRightCoors = thisLevelCoors.Where(coor => coor.X > refCoor.X && coor.Y < refCoor.Y);
-			var bottomLeftCoors = thisLevelCoors.Where(coor => coor.X < refCoor.X && coor.Y < refCoor.Y);
-			var topLeftCoors = thisLevelCoors.Where(coor => coor.X < refCoor.X && coor.Y > refCoor.Y);
+			var nextLevelCoors = new List<Coordinate>();
 
-			List<(int X, int Y)> nextLevelCoors = new List<(int X, int Y)>();
-
-			if (cornerTopCoor != default)
-			{
-				nextLevelCoors.AddRange(NeighboringCoorsToCheckAtCornerTop
-					.Where(coorShift => map[
-						cornerTopCoor.X + coorShift.X,
-						cornerTopCoor.Y + coorShift.Y])
-					.Select(coorShift => (
-						cornerTopCoor.X + coorShift.X,
-						cornerTopCoor.Y + coorShift.Y)));
-			}
-			if (cornerRightCoor != default)
-			{
-				nextLevelCoors.AddRange(NeighboringCoorsToCheckAtCornerRight
-					.Where(coorShift => map[
-						cornerRightCoor.X + coorShift.X,
-						cornerRightCoor.Y + coorShift.Y])
-					.Select(coorShift => (
-						cornerRightCoor.X + coorShift.X,
-						cornerRightCoor.Y + coorShift.Y)));
-			}
-			if (cornerBottomCoor != default)
-			{
-				nextLevelCoors.AddRange(NeighboringCoorsToCheckAtCornerBottom
-					.Where(coorShift => map[
-						cornerBottomCoor.X + coorShift.X,
-						cornerBottomCoor.Y + coorShift.Y])
-					.Select(coorShift => (
-						cornerBottomCoor.X + coorShift.X,
-						cornerBottomCoor.Y + coorShift.Y)));
-			}
-			if (cornerLeftCoor != default)
-			{
-				nextLevelCoors.AddRange(NeighboringCoorsToCheckAtCornerLeft
-					.Where(coorShift => map[
-						cornerLeftCoor.X + coorShift.X,
-						cornerLeftCoor.Y + coorShift.Y])
-					.Select(coorShift => (
-						cornerLeftCoor.X + coorShift.X,
-						cornerLeftCoor.Y + coorShift.Y)));
-			}
-
-			if (topRightCoors.Any())
-			{
-				nextLevelCoors.AddRange(topRightCoors
-					.SelectMany(coor => NeighboringCoorsToCheckAtTopRightEdge
-						.Select(coorShift => (
-							coor.X + coorShift.X,
-							coor.Y + coorShift.Y)))
-					.Where(shiftedCoor => map[shiftedCoor.Item1, shiftedCoor.Item2]));
-			}
-			if (bottomRightCoors.Any())
-			{
-				nextLevelCoors.AddRange(bottomRightCoors
-					.SelectMany(coor => NeighboringCoorsToCheckAtBottomRightEdge
-						.Select(coorShift => (
-							coor.X + coorShift.X,
-							coor.Y + coorShift.Y)))
-					.Where(shiftedCoor => map[shiftedCoor.Item1, shiftedCoor.Item2]));
-			}
-			if (bottomLeftCoors.Any())
-			{
-				nextLevelCoors.AddRange(bottomLeftCoors
-					.SelectMany(coor => NeighboringCoorsToCheckAtBottomLeftEdge
-						.Select(coorShift => (
-							coor.X + coorShift.X,
-							coor.Y + coorShift.Y)))
-					.Where(shiftedCoor => map[shiftedCoor.Item1, shiftedCoor.Item2]));
-			}
-			if (topLeftCoors.Any())
-			{
-				nextLevelCoors.AddRange(topLeftCoors
-					.SelectMany(coor => NeighboringCoorsToCheckAtTopLeftEdge
-						.Select(coorShift => (
-							coor.X + coorShift.X,
-							coor.Y + coorShift.Y)))
-					.Where(shiftedCoor => map[shiftedCoor.Item1, shiftedCoor.Item2]));
-			}
+			foreach (var searchScope in coorsToSearchFromByCoorShift)
+            {
+				nextLevelCoors.AddRange(searchScope.Value
+					.Where(coor => map[coor.X + searchScope.Key.X, coor.Y + searchScope.Key.Y])
+					.Select(coor => new Coordinate(coor.X + searchScope.Key.X, coor.Y + searchScope.Key.Y)));
+            }
 
 			return thisLevelCoors.Count()
 				+ nextLevelCoors.Distinct().GetBasinSize(map, refCoor);
