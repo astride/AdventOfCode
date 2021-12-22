355603ed467bde9bd89d96fd5dcc2c553ca312ae
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-22 12:03:57 +0100

    Part 1

--- Source/AdventOfCode.2021/Day21/Solver.cs
+++ Source/AdventOfCode.2021/Day21/Solver.cs
@@ -1,6 +1,4 @@
 ﻿using AdventOfCode.Common;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Y2021
@@ -12,17 +10,97 @@ namespace AdventOfCode.Y2021
 
 		public void SolvePuzzle(string[] rawInput)
 		{
-			Part1Solution = SolvePart1(rawInput).ToString();
+			var startingPosition1 = 
+				int.Parse(rawInput
+					.Single(entry => entry.Contains("Player 1"))
+					.Split(' ')
+					.Last());
+
+			var startingPosition2 =
+				int.Parse(rawInput
+					.Single(entry => entry.Contains("Player 2"))
+					.Split(' ')
+					.Last());
+
+			Part1Solution = SolvePart1(startingPosition1, startingPosition2).ToString();
 		}
 
-		private static int SolvePart1(string[] input)
+		private static int SolvePart1(int startingPosition1, int startingPosition2)
 		{
-			return -1;
+			var totalDieRolls = Day21Helpers.PracticeDiracDice(startingPosition1, startingPosition2, out var losingScore);
+
+			return totalDieRolls * losingScore;
 		}
 	}
 
 	public static class Day21Helpers
 	{
+		private const int Goal = 1000;
+		private const int DeterministicDieMax = 100;
+		private const int BoardMax = 10;
+		private const int RollsPerTurn = 3;
 
+		public static int PracticeDiracDice(int startingPosition1, int startingPosition2, out int losingScore)
+		{
+			var turn = 0;
+			var totalDieRolls = 0;
+
+			var score1 = 0;
+			var score2 = 0;
+
+			var position1 = startingPosition1;
+			var position2 = startingPosition2;
+
+			while (true)
+			{
+				// Player 1's turn
+				turn++;
+				totalDieRolls += RollsPerTurn;
+
+				position1 = GetBoardSpot(position1 + GetDeterministicDieSum(turn));
+				score1 += position1;
+
+				if (score1 >= Goal)
+				{
+					losingScore = score2;
+					return totalDieRolls;
+				}
+
+				// Player 2's turn
+				turn++;
+				totalDieRolls += RollsPerTurn;
+
+				position2 = GetBoardSpot(position2 + GetDeterministicDieSum(turn));
+				score2 += position2;
+
+				if (score2 >= Goal)
+				{
+					losingScore = score1;
+					return totalDieRolls;
+				}
+			}
+		}
+
+		private static int GetDeterministicDieSum(int turn)
+		{
+			var baseScore = RollsPerTurn * (turn - 1);
+
+			var dieSum =
+				GetDeterministicDieValue(baseScore + 1) +
+				GetDeterministicDieValue(baseScore + 2) +
+				GetDeterministicDieValue(baseScore + 3);
+
+			return dieSum;
+		}
+
+		private static int GetDeterministicDieValue(int absoluteValue)
+		{
+			return 1 + ((absoluteValue - 1) % DeterministicDieMax);
+		}
+
+		private static int GetBoardSpot(int score)
+		{
+			return 1 + ((score - 1) % BoardMax);
+		}
 	}
 }
