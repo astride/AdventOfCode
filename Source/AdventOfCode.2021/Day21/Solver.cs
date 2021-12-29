f867a2a48920c847a4db859b5128de92b74477ee
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2021-12-29 09:23:31 +0100

    Part 2

--- Source/AdventOfCode.2021/Day21/Solver.cs
+++ Source/AdventOfCode.2021/Day21/Solver.cs
@@ -155,162 +155,73 @@ namespace AdventOfCode.Y2021
 
 		public static double PlayDiracDie(int startingPosition1, int startingPosition2)
 		{
-			PlayDiracDieRoundPlayer1(startingPosition1, startingPosition2, 0, 0);
+			foreach (var dieSum in UniverseCountForDiracDieSum.Keys)
+			{
+				PlayDiracDieInUniverse(dieSum, startingPosition1, startingPosition2);
+			}
 
 			return Math.Max(DiracDieWinCountPlayer1, DiracDieWinCountPlayer2);
 		}
 
-		private static void PlayDiracDieRoundPlayer1(int position1, int position2, int score1, int score2)
+		private static void PlayDiracDieInUniverse(int dieSum, int position1, int position2, int score1 = 0, int score2 = 0)
 		{
-			PlayTurnPlayer1(DiracDieSum[111], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[112], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[113], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[121], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[122], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[123], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[131], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[132], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[133], position1, position2, score1, score2);
-			
-			PlayTurnPlayer1(DiracDieSum[211], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[212], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[213], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[221], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[222], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[223], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[231], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[232], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[233], position1, position2, score1, score2);
-			
-			PlayTurnPlayer1(DiracDieSum[311], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[312], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[313], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[321], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[322], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[323], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[331], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[332], position1, position2, score1, score2);
-			PlayTurnPlayer1(DiracDieSum[333], position1, position2, score1, score2);
-		}
-
-		private static void PlayDiracDieRoundPlayer2(int position1, int position2, int score1, int score2)
-		{
-			PlayTurnPlayer2(DiracDieSum[111], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[112], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[113], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[121], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[122], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[123], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[131], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[132], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[133], position1, position2, score1, score2);
-			
-			PlayTurnPlayer2(DiracDieSum[211], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[212], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[213], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[221], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[222], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[223], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[231], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[232], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[233], position1, position2, score1, score2);
-			
-			PlayTurnPlayer2(DiracDieSum[311], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[312], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[313], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[321], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[322], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[323], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[331], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[332], position1, position2, score1, score2);
-			PlayTurnPlayer2(DiracDieSum[333], position1, position2, score1, score2);
-		}
-
-		private static void PlayTurnPlayer1(int stepCount, int position1, int position2, int score1, int score2)
-		{
-			position1 = GetBoardSpot(position1 + stepCount);
+			// Player 1 always starts
+			position1 = GetBoardSpot(position1 + dieSum);
 			score1 += position1;
 
-			if (score1 >= DiracDieGoal)
+			var previousTurnRolls = new List<int> { dieSum };
+
+			foreach (var nextDieSum in UniverseCountForDiracDieSum.Keys)
 			{
-				DiracDieWinCountPlayer1++;
-				return;
+				previousTurnRolls.PlayDiracDieInUniverse(nextDieSum, position1, position2, score1, score2);
 			}
-
-			PlayDiracDieRoundPlayer2(position1, position2, score1, score2);
 		}
 
-		private static void PlayTurnPlayer2(int stepCount, int position1, int position2, int score1, int score2)
+		private static void PlayDiracDieInUniverse(this IEnumerable<int> previousDieSums, int dieSum, 
+			int position1, int position2, int score1, int score2)
 		{
-			position2 = GetBoardSpot(position2 + stepCount);
-			score2 += position2;
-
-			if (score2 >= DiracDieGoal)
+			// Player 1's turn if count of previous die sums is even; player 2's turn otherwise
+			if (previousDieSums.Count() % 2 == 0)
 			{
-				DiracDieWinCountPlayer2++;
-				return;
-			}
+				position1 = GetBoardSpot(position1 + dieSum);
+				score1 += position1;
 
-			PlayDiracDieRoundPlayer1(position1, position2, score1, score2);
-		}
-
-		/*
-		public static double PlayDiracDie(int startingPosition1, int startingPosition2)
-		{
-			PlayDiracDieTurnForPlayer1(startingPosition1, startingPosition2, 0, 0);
-
-			return Math.Max(DiracDieWinCountPlayer1, DiracDieWinCountPlayer2);
-		}
-
-		private static void PlayDiracDieTurnForPlayer1(int position1, int position2, int score1, int score2)
-		{
-			foreach (var universeCountForDieSum in UniverseCountForDiracDieSum)
-			{
-				foreach (var _ in Enumerable.Range(1, universeCountForDieSum.Value))
+				if (score1 >= DiracDieGoal)
 				{
-					CreateUniverseForPlayer1(universeCountForDieSum.Key, position1, position2, score1, score2);
+					UpdateDiracDieWinCountForPlayer1(previousDieSums.Append(dieSum));
+					return;
 				}
 			}
-		}
-
-		private static void PlayDiracDieTurnForPlayer2(int position1, int position2, int score1, int score2)
-		{
-			foreach (var universeCountForDieSum in UniverseCountForDiracDieSum)
+			else
 			{
-				foreach (var _ in Enumerable.Range(1, universeCountForDieSum.Value))
+				position2 = GetBoardSpot(position2 + dieSum);
+				score2 += position2;
+
+				if (score2 >= DiracDieGoal)
 				{
-					CreateUniverseForPlayer2(universeCountForDieSum.Key, position1, position2, score1, score2);
+					UpdateDiracDieWinCountForPlayer2(previousDieSums.Append(dieSum));
+					return;
 				}
 			}
-		}
 
-		private static void CreateUniverseForPlayer1(int diracDieSum, int position1, int position2, int score1, int score2)
-		{
-			position1 = GetBoardSpot(position1 + diracDieSum);
-			score1 += position1;
-
-			if (score1 >= DiracDieGoal)
+			foreach (var nextDieSum in UniverseCountForDiracDieSum.Keys)
 			{
-				DiracDieWinCountPlayer1++;
-				return;
+				previousDieSums.Append(dieSum).PlayDiracDieInUniverse(nextDieSum, position1, position2, score1, score2);
 			}
-
-			PlayDiracDieTurnForPlayer2(position1, position2, score1, score2);
 		}
 
-		private static void CreateUniverseForPlayer2(int diracDieSum, int position1, int position2, int score1, int score2)
+		private static void UpdateDiracDieWinCountForPlayer1(IEnumerable<int> dieSums)
 		{
-			position2 = GetBoardSpot(position2 + diracDieSum);
-			score2 += position2;
-
-			if (score2 >= DiracDieGoal)
-			{
-				DiracDieWinCountPlayer2++;
-				return;
-			}
-
-			PlayDiracDieTurnForPlayer1(position1, position2, score1, score2);
+			DiracDieWinCountPlayer1 += dieSums
+				.Select(ds => UniverseCountForDiracDieSum[ds])
+				.Aggregate((double)1, (total, next) => total * next);
 		}
-		*/
+
+		private static void UpdateDiracDieWinCountForPlayer2(IEnumerable<int> dieSums)
+		{
+			DiracDieWinCountPlayer2 += dieSums
+				.Select(ds => UniverseCountForDiracDieSum[ds])
+				.Aggregate((double)1, (total, next) => total * next);
+		}
 	}
 }
