9e730e283c87281d0f4cab5b564992e90bf45ffe
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2022-02-21 20:12:04 +0100

    Migrating from .NET Framework 4.8 --> .NET 6

---
+++ Source/AdventOfCode.CommonB/IPuzzleSolver.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode.Common
+{
+	public interface IPuzzleSolver
+	{
+		string? Part1Solution { get; set; }
+
+		string? Part2Solution { get; set; }
+		
+		void SolvePuzzle(string[] input);
+	}
+}
