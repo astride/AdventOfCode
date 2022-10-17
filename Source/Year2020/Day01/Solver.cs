f3f6257ad2bf8e4636fb87103ded64344e41f723
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2022-10-17 07:12:36 +0200

    Year2020 project: Cleaning up code warnings (except for warning "Namespace does not correspond to file location")

--- Source/Year2020/Day01/Solver.cs
+++ Source/Year2020/Day01/Solver.cs
@@ -4,13 +4,13 @@ namespace Year2020;
 
 public class Day01Solver : IPuzzleSolver
 {
-    public string Part1Solution { get; set; }
-    public string Part2Solution { get; set; }
+    public string Part1Solution { get; set; } = string.Empty;
+    public string Part2Solution { get; set; } = string.Empty;
 
     public void SolvePuzzle(string[] input)
     {
         var report = input
-            .Select(entry => int.Parse(entry))
+            .Select(int.Parse)
             .ToArray();
 
         Part1Solution = SolvePart1(report).ToString();
@@ -19,18 +19,15 @@ public class Day01Solver : IPuzzleSolver
 
     private const int Sum = 2020;
 
-    private int SolvePart1(int[] report)
+    private static int SolvePart1(IReadOnlyList<int> report)
     {
-        int expense1;
-        int expense2;
-
-        for (var i1 = 0; i1 < report.Length - 1; i1++)
+        for (var i1 = 0; i1 < report.Count - 1; i1++)
         {
-            expense1 = report[i1];
+            var expense1 = report[i1];
 
-            for (var i2 = i1 + 1; i2 < report.Length; i2++)
+            for (var i2 = i1 + 1; i2 < report.Count; i2++)
             {
-                expense2 = report[i2];
+                var expense2 = report[i2];
 
                 if (expense1 + expense2 == Sum)
                 {
@@ -42,23 +39,19 @@ public class Day01Solver : IPuzzleSolver
         return -1;
     }
 
-    private int SolvePart2(int[] report)
+    private static int SolvePart2(IReadOnlyList<int> report)
     {
-        int expense1;
-        int expense2;
-        int expense3;
-
-        for (var i1 = 0; i1 < report.Length - 2; i1++)
+        for (var i1 = 0; i1 < report.Count - 2; i1++)
         {
-            expense1 = report[i1];
+            var expense1 = report[i1];
 
-            for (var i2 = i1 + 1; i2 < report.Length - 1; i2++)
+            for (var i2 = i1 + 1; i2 < report.Count - 1; i2++)
             {
-                expense2 = report[i2];
+                var expense2 = report[i2];
 
-                for (var i3 = i2 + 1; i3 < report.Length; i3++)
+                for (var i3 = i2 + 1; i3 < report.Count; i3++)
                 {
-                    expense3 = report[i3];
+                    var expense3 = report[i3];
 
                     if (expense1 + expense2 + expense3 == Sum)
                     {
