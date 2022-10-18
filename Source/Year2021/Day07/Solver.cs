b701ff782d8bc7ef70833998a184363695937cf1
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2022-10-18 13:04:50 +0200

    Year2021 project: Cleaning up code in Day07

--- Source/Year2021/Day07/Solver.cs
+++ Source/Year2021/Day07/Solver.cs
@@ -1,45 +1,34 @@
-﻿using Common.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Globalization;
+using Common.Interfaces;
 
 namespace Year2021;
 
 public class Day07Solver : IPuzzleSolver
 {
-	public string Part1Solution { get; set; }
-	public string Part2Solution { get; set; }
+	public string Part1Solution { get; set; } = string.Empty;
+	public string Part2Solution { get; set; } = string.Empty;
 
 	public void SolvePuzzle(string[] rawInput)
 	{
 		var input = rawInput
-			.Where(entry => !string.IsNullOrWhiteSpace(entry))
-			.Single()
+			.Single(entry => !string.IsNullOrWhiteSpace(entry))
 			.Split(',')
-			.Select(entry => int.Parse(entry))
+			.Select(int.Parse)
 			.ToArray();
 
 		Part1Solution = SolvePart1(input).ToString();
-		Part2Solution = SolvePart2(input).ToString();
+		Part2Solution = SolvePart2(input).ToString(CultureInfo.InvariantCulture);
 	}
 
-	private static int SolvePart1(int[] crabPositions)
+	private static int SolvePart1(IReadOnlyCollection<int> crabPositions)
 	{
 		var minPos = crabPositions.Min();
 		var maxPos = crabPositions.Max();
 
-		var cheapestFuel = crabPositions.Length * (maxPos - minPos); // Max possible spent fuel
-		int requiredFuel;
-
-		foreach (var finalPos in Enumerable.Range(minPos, maxPos))
-		{
-			requiredFuel = crabPositions.Select(pos => Math.Abs(pos - finalPos)).Sum();
-
-			if (requiredFuel < cheapestFuel)
-			{
-				cheapestFuel = requiredFuel;
-			}
-		}
+		var cheapestFuel = Enumerable.Range(minPos, maxPos - minPos + 1)
+			.Select(finalPos => crabPositions
+				.Sum(pos => Math.Abs(finalPos - pos)))
+			.Min();
 
 		return cheapestFuel;
 	}
@@ -53,23 +42,21 @@ public class Day07Solver : IPuzzleSolver
 
 		var minFinalPosition = crabPositions.Min();
 		var maxFinalPosition = crabPositions.Max();
-		var possibleFinalPositions = Enumerable.Range(minFinalPosition, (maxFinalPosition - minFinalPosition) + 1);
-		
-		var startPositionIndices = Enumerable.Range(0, positionAndCrabCount.Count());
 
-		var requiredStepsFromPosition = new List<int>();
-		double requiredFuel;
+		var possibleFinalPositions =
+			Enumerable.Range(minFinalPosition, (maxFinalPosition - minFinalPosition) + 1).ToList();
 
 		var cheapestFuel = double.MaxValue;
 
-		foreach (var position in possibleFinalPositions)
+		foreach (var finalPos in possibleFinalPositions)
 		{
-			requiredStepsFromPosition = positionAndCrabCount
-				.Select(pacc => Math.Abs(pacc.Position - position))
-				.ToList();
+			var requiredStepsFromPosition = positionAndCrabCount
+				.Select(pacc => pacc.Position)
+				.Select(pos => Math.Abs(pos - finalPos));
 
-			requiredFuel = startPositionIndices
-				.Select(i => positionAndCrabCount[i].Count * requiredStepsFromPosition[i].CalculateSpentFuel())
+			var requiredFuel = requiredStepsFromPosition
+				.Zip(positionAndCrabCount.Select(pacc => pacc.Count),
+					(requiredSteps, crabCount) => crabCount * requiredSteps.CalculateSpentFuel())
 				.Sum();
 
 			if (requiredFuel < cheapestFuel)
@@ -82,12 +69,15 @@ public class Day07Solver : IPuzzleSolver
 	}
 }
 
-public static class Day07Helpers
+internal static class Day07Helpers
 {
 	public static double CalculateSpentFuel(this int steps)
 	{
-		if (steps == 0) return 0;
+		if (steps == 0)
+		{
+			return 0;
+		}
 
-		return (steps * (steps + 1) / 2);
+		return (double)steps * (steps + 1) / 2;
 	}
 }
