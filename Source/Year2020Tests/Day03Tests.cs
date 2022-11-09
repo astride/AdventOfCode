9079322e61e4af2f8990f4e6a21a3b6c5e0cc4fa
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2022-11-09 19:21:32 +0100

    [ 2020 - 03 ] Correcting the expected test results for part 2

--- Source/Year2020Tests/Day03Tests.cs
+++ Source/Year2020Tests/Day03Tests.cs
@@ -7,9 +7,9 @@ public class Day03Tests : TestsBase
 {
     protected override string DirectoryName => "Day03";
     protected override string Part1Solution => "214";
-    protected override string Part2Solution => "336";
+    protected override string Part2Solution => "8336352024";
     protected override string Part1ExampleSolution => "7";
-    protected override string Part2ExampleSolution => "8336352024";
+    protected override string Part2ExampleSolution => "336";
 
     public Day03Tests()
     {
