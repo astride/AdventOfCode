15a8ac7be706d6662f14446da2e36c264594df24
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-01 06:32:42 +0100

    [ 2022 - 01 ]

--- Source/Year2022/Day01/Solver.cs
+++ Source/Year2022/Day01/Solver.cs
@@ -4,25 +4,58 @@ namespace Year2022;
 
 public class Day01Solver : IPuzzleSolver
 {
-    public string Title => string.Empty;
+    public string Title => "Calorie Counting";
     public string Part1Solution { get; set; } = string.Empty;
     public string Part2Solution { get; set; } = string.Empty;
 
     public void SolvePuzzle(string[] input)
     {
-        var puzzleInput = new[] { 1, 2, 3 };
+        var puzzleInput = input.GetCaloriesPerElf().ToArray();
 
         Part1Solution = SolvePart1(puzzleInput).ToString();
         Part2Solution = SolvePart2(puzzleInput).ToString();
     }
 
-    private static int SolvePart1(IReadOnlyList<int> input)
+    private static int SolvePart1(IEnumerable<int> caloriesPerElf)
     {
-        return 0;
+        return caloriesPerElf.Max();
     }
 
-    private static int SolvePart2(IReadOnlyList<int> input)
+    private static int SolvePart2(IEnumerable<int> caloriesPerElf)
     {
-        return 0;
+        var topThreeWithMaxCalories = caloriesPerElf
+            .OrderByDescending(cal => cal)
+            .Take(3);
+        
+        return topThreeWithMaxCalories.Sum();
+    }
+}
+
+internal static class Extensions
+{
+    public static IEnumerable<int> GetCaloriesPerElf(this IEnumerable<string> input)
+    {
+        var calories = 0;
+
+        foreach (var item in input)
+        {
+            if (string.IsNullOrWhiteSpace(item))
+            {
+                if (calories > 0)
+                {
+                    yield return calories;
+                    calories = 0;
+                }
+
+                continue;
+            }
+
+            calories += int.Parse(item);
+        }
+
+        if (calories > 0)
+        {
+            yield return calories;
+        }
     }
 }
