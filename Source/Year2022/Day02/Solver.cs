07f69d31e20f859d5df03f129dfff6f90a10f290
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-02 08:02:28 +0100

    [ 2022 - 02 ]

--- Source/Year2022/Day02/Solver.cs
+++ Source/Year2022/Day02/Solver.cs
@@ -4,29 +4,109 @@ namespace Year2022;
 
 public class Day02Solver : IPuzzleSolver
 {
-    public string Title => ""; // TODO
+    public string Title => "Rock Paper Scissors";
     
     public string Part1Solution { get; set; } = string.Empty;
     public string Part2Solution { get; set; } = string.Empty;
 
-    public void SolvePuzzle(string[] input)
+    public void SolvePuzzle(string[] encryptedStrategyGuide)
     {
-        var puzzleInput = input
-            .Where(item => !string.IsNullOrEmpty(item))
-            .Select(int.Parse)
-            .ToArray();
-
-        Part1Solution = SolvePart1(puzzleInput).ToString();
-        //Part2Solution = SolvePart2(puzzleInput).ToString();
+        Part1Solution = SolvePart1(encryptedStrategyGuide).ToString();
+        Part2Solution = SolvePart2(encryptedStrategyGuide).ToString();
     }
 
-    private static int SolvePart1(IEnumerable<int> input)
+    private static int SolvePart1(IEnumerable<string> encryptedStrategyGuide)
     {
-        return 0;
+        var rounds = encryptedStrategyGuide.GetRoundsForPart1();
+        
+        return rounds.GetScore();
     }
 
-    private static int SolvePart2(IEnumerable<int> input)
+    private static int SolvePart2(IEnumerable<string> encryptedStrategyGuide)
     {
-        return 0;
+        var rounds = encryptedStrategyGuide.GetRoundsForPart2();
+
+        return rounds.GetScore();
     }
 }
+
+internal static class Day02Extensions
+{
+    public static IEnumerable<Round> GetRoundsForPart1(this IEnumerable<string> guide)
+    {
+        var roundPerGuideline = new Dictionary<string, Round>
+        {
+            ["A X"] = new(Shape.Rock, Outcome.Draw),
+            ["A Y"] = new(Shape.Paper, Outcome.Win),
+            ["A Z"] = new(Shape.Scissor, Outcome.Loss),
+            ["B X"] = new(Shape.Rock, Outcome.Loss),
+            ["B Y"] = new(Shape.Paper, Outcome.Draw),
+            ["B Z"] = new(Shape.Scissor, Outcome.Win),
+            ["C X"] = new(Shape.Rock, Outcome.Win),
+            ["C Y"] = new(Shape.Paper, Outcome.Loss),
+            ["C Z"] = new(Shape.Scissor, Outcome.Draw),
+        };
+
+        return guide
+            .Select(guideline => roundPerGuideline[guideline])
+            .ToList();
+    }
+    
+    public static IEnumerable<Round> GetRoundsForPart2(this IEnumerable<string> guide)
+    {
+        var roundPerGuideline = new Dictionary<string, Round>
+        {
+            ["A X"] = new(Shape.Scissor, Outcome.Loss),
+            ["A Y"] = new(Shape.Rock, Outcome.Draw),
+            ["A Z"] = new(Shape.Paper, Outcome.Win),
+            ["B X"] = new(Shape.Rock, Outcome.Loss),
+            ["B Y"] = new(Shape.Paper, Outcome.Draw),
+            ["B Z"] = new(Shape.Scissor, Outcome.Win),
+            ["C X"] = new(Shape.Paper, Outcome.Loss),
+            ["C Y"] = new(Shape.Scissor, Outcome.Draw),
+            ["C Z"] = new(Shape.Rock, Outcome.Win),
+        };
+
+        return guide
+            .Select(guideline => roundPerGuideline[guideline])
+            .ToList();
+    }
+
+    public static int GetScore(this IEnumerable<Round> rounds)
+    {
+        var scoreByShape = new Dictionary<Shape, int>
+        {
+            [Shape.Rock] = 1,
+            [Shape.Paper] = 2,
+            [Shape.Scissor] = 3,
+        };
+
+        var scoreByOutcome = new Dictionary<Outcome, int>
+        {
+            [Outcome.Loss] = 0,
+            [Outcome.Draw] = 3,
+            [Outcome.Win] = 6,
+        };
+
+        return rounds
+            .Sum(round => scoreByShape[round.Shape] + scoreByOutcome[round.Outcome]);
+    }
+}
+
+internal enum Outcome
+{
+    Loss,
+    Draw,
+    Win,
+}
+
+internal enum Shape
+{
+    Rock,
+    Scissor,
+    Paper,
+}
+
+internal record Round(
+    Shape Shape,
+    Outcome Outcome);
--- Source/Year2022Tests/Day02Tests.cs
+++ Source/Year2022Tests/Day02Tests.cs
@@ -6,19 +6,19 @@ namespace Year2022Tests;
 public class Day02Tests : TestsBase
 {
     protected override string DirectoryName => "Day02";
-    protected override string Part1Solution => "";
-    protected override string Part2Solution => "";
-    protected override string Part1ExampleSolution => "";
-    protected override string Part2ExampleSolution => "";
+    protected override string Part1Solution => "10595";
+    protected override string Part2Solution => "9541";
+    protected override string Part1ExampleSolution => "15";
+    protected override string Part2ExampleSolution => "12";
 
     public Day02Tests()
     {
-        PuzzleSolver = new Day01Solver();
+        PuzzleSolver = new Day02Solver();
     }
     
     [TestMethod]
-    public void Day01IsValid() => HasCorrectSolutions();
+    public void Day02IsValid() => HasCorrectSolutions();
 
     [TestMethod]
-    public void Day01IsValid_WithExampleInput() => HasCorrectSolutionsWithExampleInput();
+    public void Day02IsValid_WithExampleInput() => HasCorrectSolutionsWithExampleInput();
 }
