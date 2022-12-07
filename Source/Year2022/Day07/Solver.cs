4580ba3a443d0798c4049de96c18bf4713535a62
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-07 19:52:58 +0100

    [ 2022 - 07 ] Unsuccessful implementation

--- Source/Year2022/Day07/Solver.cs
+++ Source/Year2022/Day07/Solver.cs
@@ -15,9 +15,91 @@ public class Day07Solver : IPuzzleSolver
         //Part2Solution = SolvePart2(input).ToString();
     }
 
-    private static int SolvePart1(IEnumerable<string> input)
+    private static double SolvePart1(IEnumerable<string> terminalOutput)
     {
-        return 0;
+        const string changeDirCommandPrefix = "$ cd";
+        const string goToParentDirCommand = changeDirCommandPrefix + " ..";
+        const string listContent = "$ ls";
+        const string dirPrefix = "dir";
+        
+        const double maxSizeOfRelevantDirectories = 100000;
+
+        var directoryStack = new Stack<string>();
+        var totalFileSizeByDirName = new Dictionary<string, double>();
+
+        string GetDirPath() => string.Join("/", directoryStack);
+        
+        bool IsFileInfo(string line) => int.TryParse(line[..1], out _);
+
+        var terminalLinesOfInterest = terminalOutput
+            .Where(line => !line.StartsWith(dirPrefix))
+            .Where(line => line != listContent);
+
+        foreach (var line in terminalLinesOfInterest)
+        {
+            Console.WriteLine();
+            Console.WriteLine(line);
+
+            if (line == goToParentDirCommand)
+            {
+                directoryStack.Pop();
+                
+                Console.WriteLine("Current dir: '" + GetDirPath() + "'");
+                
+                continue;
+            }
+            
+            if (line.StartsWith(changeDirCommandPrefix))
+            {
+                var dirName = line.Split(' ')[2];
+
+                if (dirName == "/")
+                {
+                    dirName = "root";
+                }
+                
+                directoryStack.Push(dirName);
+                
+                Console.WriteLine("Change to dir: '" + GetDirPath() + "'");
+
+                continue;
+            }
+            
+            if (IsFileInfo(line))
+            {
+                var fileSize = double.Parse(line.Split(' ')[0]);
+                
+                Console.WriteLine("'" + GetDirPath() + "' contains a file of size " + fileSize);
+
+                foreach (var dir in directoryStack)
+                {
+                    if (totalFileSizeByDirName.ContainsKey(dir))
+                    {
+                        var previousTotalFileSize = totalFileSizeByDirName[dir];
+                        totalFileSizeByDirName[dir] += fileSize;
+                        
+                        Console.WriteLine("--> '" + dir + "' total file size: (" + previousTotalFileSize + " + " + fileSize + " = )" + totalFileSizeByDirName[dir]);
+                    }
+                    else
+                    {
+                        totalFileSizeByDirName[dir] = fileSize;
+                        
+                        Console.WriteLine("--> '" + dir + "' total file size: " + totalFileSizeByDirName[dir]);
+                    }
+                }
+
+                continue;
+            }
+        }
+        
+        var totalFileSizesOfRelevantDirectories = totalFileSizeByDirName
+            .Values
+            .Where(val => val <= maxSizeOfRelevantDirectories);
+
+        var totalRelevantDirectoryFileSize = totalFileSizesOfRelevantDirectories.Sum();
+        
+        // 924098: Too low
+        return totalRelevantDirectoryFileSize;
     }
 
     private static int SolvePart2(IEnumerable<string> input)
--- Source/Year2022Tests/Day07Tests.cs
+++ Source/Year2022Tests/Day07Tests.cs
@@ -8,7 +8,7 @@ public class Day07Tests : TestsBase
     protected override string DirectoryName => "Day07";
     protected override string Part1Solution => "";
     protected override string Part2Solution => "";
-    protected override string Part1ExampleSolution => "";
+    protected override string Part1ExampleSolution => "95437";
     protected override string Part2ExampleSolution => "";
 
     public Day07Tests()
