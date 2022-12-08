a70883dc747fe65f8f1cc6954060880ba883a278
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-08 16:46:47 +0100

    [ 2022 - 07 ] Updating tests with solutions

--- Source/Year2022Tests/Day07Tests.cs
+++ Source/Year2022Tests/Day07Tests.cs
@@ -6,10 +6,10 @@ namespace Year2022Tests;
 public class Day07Tests : TestsBase
 {
     protected override string DirectoryName => "Day07";
-    protected override string Part1Solution => "";
-    protected override string Part2Solution => "";
+    protected override string Part1Solution => "1118405";
+    protected override string Part2Solution => "12545514";
     protected override string Part1ExampleSolution => "95437";
-    protected override string Part2ExampleSolution => "";
+    protected override string Part2ExampleSolution => "24933642";
 
     public Day07Tests()
     {
