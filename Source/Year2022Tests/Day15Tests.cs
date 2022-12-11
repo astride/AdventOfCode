f305682662791e7f194d1abefde6079b1f5d92d5
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-11 19:36:24 +0100

    [ 2022 - 15 ] Preparations

---
+++ Source/Year2022Tests/Day15Tests.cs
@@ -0,0 +1,24 @@
+using Year2022;
+
+namespace Year2022Tests;
+
+[TestClass]
+public class Day15Tests : TestsBase
+{
+    protected override string DirectoryName => "Day15";
+    protected override string Part1ExampleSolution => "";
+    protected override string Part1Solution => "";
+    protected override string Part2ExampleSolution => "";
+    protected override string Part2Solution => "";
+
+    public Day15Tests()
+    {
+        PuzzleSolver = new Day15Solver();
+    }
+    
+    [TestMethod]
+    public void Day15IsValid() => HasCorrectSolutions();
+
+    [TestMethod]
+    public void Day15IsValid_WithExampleInput() => HasCorrectSolutionsWithExampleInput();
+}
