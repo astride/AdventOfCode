a520f42a16b6336fac9f4d9c6304b7e0837467ac
astride <6726401+astride@users.noreply.github.com>
2022-12-11 19:29:32 +0100

    [ 2022 ] Adjusting the templates (#39)

--- Source/Year2022/Template/Solver.cs
+++ Source/Year2022/Template/Solver.cs
@@ -11,16 +11,16 @@ public class Day00Solver : IPuzzleSolver
 
     public void SolvePuzzle(string[] input)
     {
-        Part1Solution = SolvePart1(input).ToString();
-        Part2Solution = SolvePart2(input).ToString();
+        Part1Solution = GetPart1Solution(input).ToString();
+        Part2Solution = GetPart2Solution(input).ToString();
     }
 
-    private static int SolvePart1(IEnumerable<string> input)
+    private static int GetPart1Solution(IEnumerable<string> input)
     {
         return 0;
     }
 
-    private static int SolvePart2(IEnumerable<string> input)
+    private static int GetPart2Solution(IEnumerable<string> input)
     {
         return 0;
     }
