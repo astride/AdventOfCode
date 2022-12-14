b7f16d551eb945f4d6016f461f8d93c7c3dd1169
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-14 22:38:44 +0100

    [ 2022 - 13 ] Part 1

--- Source/Year2022/Day13/Solver.cs
+++ Source/Year2022/Day13/Solver.cs
@@ -4,7 +4,7 @@ namespace Year2022;
 
 public class Day13Solver : IPuzzleSolver
 {
-    public string Title => "";
+    public string Title => "Distress Signal";
 
     public string Part1Solution { get; set; } = string.Empty;
     public string Part2Solution { get; set; } = string.Empty;
@@ -17,7 +17,112 @@ public class Day13Solver : IPuzzleSolver
 
     private static int GetPart1Solution(IEnumerable<string> input)
     {
-        return 0;
+        var packetPairs = input
+            .Where(line => !string.IsNullOrEmpty(line))
+            .Chunk(2)
+            .ToList();
+
+        var packetPairIndicesInRightOrder = new HashSet<int>();
+
+        for (var i = 0; i < packetPairs.Count; i++)
+        {
+            var left = packetPairs[i].First();
+            var right = packetPairs[i].Last();
+
+            var iLeft = -1;
+            var iRight = -1;
+
+            while (true)
+            {
+                iLeft++;
+                iRight++;
+                
+                var leftChar = left[iLeft];
+                var rightChar = right[iRight];
+
+                // Hacky business here...
+                var leftIsTen = left.Length > iLeft + 3 && left[iLeft..(iLeft + 2)] == "10";
+                var rightIsTen = right.Length > iRight + 3 && right[iRight..(iRight + 2)] == "10";
+
+                if (leftIsTen)
+                {
+                    iLeft++;
+                }
+
+                if (rightIsTen)
+                {
+                    iRight++;
+                }
+
+                if (leftIsTen && rightIsTen)
+                {
+                    continue;
+                }
+
+                if (leftIsTen)
+                {
+                    break;
+                }
+
+                if (rightIsTen)
+                {
+                    packetPairIndicesInRightOrder.Add(i + 1);
+                    break;
+                }
+                
+                if (leftChar == rightChar)
+                {
+                    continue;
+                }
+
+                if (leftChar == '[' && rightChar == ']')
+                {
+                    break;
+                }
+                
+                if (leftChar == ']' && rightChar == '[')
+                {
+                    packetPairIndicesInRightOrder.Add(i + 1);
+                    break;
+                }
+
+                if (leftChar == ']')
+                {
+                    packetPairIndicesInRightOrder.Add(i + 1);
+                    break;
+                }
+
+                if (rightChar == ']')
+                {
+                    break;
+                }
+                
+                if (leftChar == '[')
+                {
+                    right = right[..iRight] + '[' + rightChar + ']' + right[(iRight + 1)..];
+                    continue;
+                }
+
+                if (rightChar == '[')
+                {
+                    left = left[..iLeft] + '[' + leftChar + ']' + left[(iLeft + 1)..];
+                    continue;
+                }
+                
+                if (leftChar > rightChar)
+                {
+                    break;
+                }
+
+                if (leftChar < rightChar)
+                {
+                    packetPairIndicesInRightOrder.Add(i + 1);
+                    break;
+                }
+            }
+        }
+        
+        return packetPairIndicesInRightOrder.Sum();
     }
 
     private static int GetPart2Solution(IEnumerable<string> input)
--- Source/Year2022Tests/Day13Tests.cs
+++ Source/Year2022Tests/Day13Tests.cs
@@ -7,7 +7,7 @@ public class Day13Tests : TestsBase
 {
     protected override string DirectoryName => "Day13";
     protected override string Part1ExampleSolution => "13"; // Sum of 1-based indices 1, 2, 4, 6
-    protected override string Part1Solution => "";
+    protected override string Part1Solution => "5252";
     protected override string Part2ExampleSolution => "";
     protected override string Part2Solution => "";
 
