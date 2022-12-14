e4b89eb2e93d32d2bd086f62ff0bd5e4d39dda61
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-14 06:52:30 +0100

    [ 2022 - 13 ] Populating input files; Populating Part1ExampleSolution in the tests file

--- Source/Year2022Tests/Day13Tests.cs
+++ Source/Year2022Tests/Day13Tests.cs
@@ -6,7 +6,7 @@ namespace Year2022Tests;
 public class Day13Tests : TestsBase
 {
     protected override string DirectoryName => "Day13";
-    protected override string Part1ExampleSolution => "";
+    protected override string Part1ExampleSolution => "13"; // Sum of 1-based indices 1, 2, 4, 6
     protected override string Part1Solution => "";
     protected override string Part2ExampleSolution => "";
     protected override string Part2Solution => "";
