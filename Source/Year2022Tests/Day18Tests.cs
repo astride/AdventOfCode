c3f889254981a1b9c8201b30d962ba2fda44250d
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-22 18:49:13 +0100

    [ 2022 - 18 ] Part 2: Adding Part2ExampleSolution to test class

--- Source/Year2022Tests/Day18Tests.cs
+++ Source/Year2022Tests/Day18Tests.cs
@@ -8,7 +8,7 @@ public class Day18Tests : TestsBase
 	protected override string DirectoryName => "Day18";
 	protected override string Part1ExampleSolution => "64";
 	protected override string Part1Solution => "4332";
-	protected override string Part2ExampleSolution => "";
+	protected override string Part2ExampleSolution => "58";
 	protected override string Part2Solution => "";
 
 	public Day18Tests()
