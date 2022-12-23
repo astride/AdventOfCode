c9e3d63437988611dede34194999270ae43b90a5
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-23 16:44:36 +0100

    [ 2022 - 23 ] Preparations (including input files)

---
+++ Source/Year2022Tests/Day23Tests.cs
@@ -0,0 +1,24 @@
+using Year2022;
+
+namespace Year2022Tests;
+
+[TestClass]
+public class Day23Tests : TestsBase
+{
+    protected override string DirectoryName => "Day23";
+    protected override string Part1ExampleSolution => "";
+    protected override string Part1Solution => "";
+    protected override string Part2ExampleSolution => "";
+    protected override string Part2Solution => "";
+
+    public Day23Tests()
+    {
+        PuzzleSolver = new Day23Solver();
+    }
+    
+    [TestMethod]
+    public void Day23IsValid() => HasCorrectSolutions();
+
+    [TestMethod]
+    public void Day23IsValid_WithExampleInput() => HasCorrectSolutionsWithExampleInput();
+}
