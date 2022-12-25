148de84b156c2e29cdca19d163966c40675b4753
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-25 13:29:34 +0100

    [ 2022 - 25 ] Part 1: Calculating the decimal value

--- Source/Year2022/Day25/Solver.cs
+++ Source/Year2022/Day25/Solver.cs
@@ -8,6 +8,15 @@ public class Day25Solver : IPuzzleSolver
 
     public string Part1Solution { get; set; } = string.Empty;
     public string Part2Solution { get; set; } = string.Empty;
+    
+    private static readonly Dictionary<char, int> DecimalDigitBySnafuDigit = new()
+    {
+        ['2'] = 2,
+        ['1'] = 1,
+        ['0'] = 0,
+        ['-'] = -1,
+        ['='] = -2,
+    }; 
 
     public void SolvePuzzle(string[] input)
     {
@@ -15,9 +24,39 @@ public class Day25Solver : IPuzzleSolver
         Part2Solution = GetPart2Solution(input).ToString();
     }
 
-    private static int GetPart1Solution(IEnumerable<string> input)
+    private static string GetPart1Solution(IEnumerable<string> input)
     {
-        return 0;
+        var fuelNeededPerHotAirBalloon = new List<double>(input.Count());
+
+        foreach (var line in input)
+        {
+            double fuelNeededForHotAirBalloon = 0;
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                var snafuPower = Math.Pow(5, i);
+                var snafuDigit = line[line.Length - 1 - i];
+                
+                fuelNeededForHotAirBalloon += snafuPower * DecimalDigitBySnafuDigit[snafuDigit];
+            }
+            
+            fuelNeededPerHotAirBalloon.Add(fuelNeededForHotAirBalloon);
+        }
+
+        var decimalSum = fuelNeededPerHotAirBalloon.Sum();
+
+        Console.WriteLine("Decimal sum is: " + decimalSum);
+
+        var snafuSum = CalculateSnafuNumber(decimalSum);
+        
+        return snafuSum;
+    }
+
+    private static string CalculateSnafuNumber(double decimalNumber)
+    {
+        // TODO
+
+        return decimalNumber.ToString();
     }
 
     private static int GetPart2Solution(IEnumerable<string> input)
