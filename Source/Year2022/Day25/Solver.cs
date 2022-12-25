0bf0307ea59157049e42ae65623aa7ddd94b46b3
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-25 16:14:50 +0100

    [ 2022 - 25 ] Part 1

--- Source/Year2022/Day25/Solver.cs
+++ Source/Year2022/Day25/Solver.cs
@@ -8,6 +8,8 @@ public class Day25Solver : IPuzzleSolver
 
     public string Part1Solution { get; set; } = string.Empty;
     public string Part2Solution { get; set; } = string.Empty;
+
+    private const int NumberSystemBase = 5;
     
     private static readonly Dictionary<char, int> DecimalDigitBySnafuDigit = new()
     {
@@ -34,7 +36,7 @@ public class Day25Solver : IPuzzleSolver
 
             for (var i = 0; i < line.Length; i++)
             {
-                var snafuPower = Math.Pow(5, i);
+                var snafuPower = Math.Pow(NumberSystemBase, i);
                 var snafuDigit = line[line.Length - 1 - i];
                 
                 fuelNeededForHotAirBalloon += snafuPower * DecimalDigitBySnafuDigit[snafuDigit];
@@ -45,18 +47,101 @@ public class Day25Solver : IPuzzleSolver
 
         var decimalSum = fuelNeededPerHotAirBalloon.Sum();
 
-        Console.WriteLine("Decimal sum is: " + decimalSum);
-
         var snafuSum = CalculateSnafuNumber(decimalSum);
+        
+        Console.WriteLine("Decimal sum is: " + decimalSum);
+        Console.WriteLine("SNAFU sum is: " + snafuSum);
         
         return snafuSum;
     }
 
     private static string CalculateSnafuNumber(double decimalNumber)
     {
-        // TODO
+        var highestNeededPotence = GetHighestNeededPotence(decimalNumber);
 
-        return decimalNumber.ToString();
+        // Account for the possibility that one higher potence needs to be used
+        var highestDigitIndex = highestNeededPotence + 1;
+        
+        const int lowestDigitIndex = 0;
+        const int maxValue = 2;
+
+        var multiplePerDigitIndex = new int[highestDigitIndex + 1];
+
+        var remainder = decimalNumber;
+
+        // Populate multiplePerDigitIndex
+        for (var i = highestDigitIndex; i >= lowestDigitIndex; i--)
+        {
+            var baseNumberForIndex = Math.Pow(NumberSystemBase, i);
+            var multipleForIndex = (int)(remainder / baseNumberForIndex);
+            
+            multiplePerDigitIndex[i] = multipleForIndex;
+
+            remainder -= multipleForIndex * baseNumberForIndex;
+        }
+
+        // If necessary: Recalculate items of multiplePerDigitIndex (starting with i = 0)
+        while (multiplePerDigitIndex.Any(mul => mul > maxValue))
+        {
+            var addendForNextDigitIndex = 0;
+
+            for (var i = lowestDigitIndex; i <= highestDigitIndex; i++)
+            {
+                var currentMultiple = multiplePerDigitIndex[i];
+
+                var multipleIsThree = currentMultiple == 3;
+                var multipleIsFour = currentMultiple == 4;
+
+                var locallyUpdatedMultiple = multipleIsFour
+                    ? -1
+                    : multipleIsThree
+                        ? -2
+                        : currentMultiple;
+
+                var newMultiple = addendForNextDigitIndex + locallyUpdatedMultiple;
+
+                if (newMultiple != currentMultiple)
+                {
+                    multiplePerDigitIndex[i] = newMultiple;
+                }
+
+                addendForNextDigitIndex = multipleIsThree || multipleIsFour ? 1 : 0;
+            }
+        }
+
+        var snafuDigitByDecimalDigit = DecimalDigitBySnafuDigit
+            .ToDictionary(
+                kvp => kvp.Value,
+                kvp => kvp.Key);
+
+        var snafuNumber = new string(multiplePerDigitIndex
+            // Order multiples by descending index order
+            .Reverse()
+            // Trim away the redundant multiples for the upper potences
+            .SkipWhile(multiple => multiple == 0)
+            // Map to SNAFU digits
+            .Select(multiple => snafuDigitByDecimalDigit[multiple])
+            .ToArray());
+
+        return snafuNumber;
+    }
+
+    private static int GetHighestNeededPotence(double decimalNumber)
+    {
+        var potence = 0;
+        double accProduct = 1;
+
+        while (true)
+        {
+            accProduct *= NumberSystemBase;
+
+            if (accProduct > decimalNumber)
+            {
+                return potence;
+            }
+
+            potence++;
+        }
     }
 
     private static int GetPart2Solution(IEnumerable<string> input)
--- Source/Year2022Tests/Day25Tests.cs
+++ Source/Year2022Tests/Day25Tests.cs
@@ -6,8 +6,8 @@ namespace Year2022Tests;
 public class Day25Tests : TestsBase
 {
     protected override string DirectoryName => "Day25";
-    protected override string Part1ExampleSolution => "2=-1=0"; // SNAFU number for decimal number 4890
-    protected override string Part1Solution => "";
+    protected override string Part1ExampleSolution => "2=-1=0"; // Equals decimal number 4890
+    protected override string Part1Solution => "20=212=1-12=200=00-1"; // Equals decimal number 36966761092496
     protected override string Part2ExampleSolution => "";
     protected override string Part2Solution => "";
 
