cdb46603c2e8b9e56ca8c161f7289172791b2b7b
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2022-12-25 21:23:14 +0100

    Common: Adjusting properties and methods of IPuzzleSolver

--- Source/Common/Interfaces/IPuzzleSolver.cs
+++ Source/Common/Interfaces/IPuzzleSolver.cs
@@ -3,17 +3,33 @@ namespace Common.Interfaces;
 public interface IPuzzleSolver
 {
     string Title { get; }
-    
-    string Part1Solution { get; set; }
 
-    string Part2Solution { get; set; }
-    
-    void SolvePuzzle(string[] input);
+    // Need not be set in implementation class
+    object? Part1Solution { get; set; }
+
+    // Need not be set in implementation class
+    object? Part2Solution { get; set; }
+
+    void SolvePuzzle(string[] input)
+    {
+        SolvePart1(input);
+        SolvePart2(input);
+    }
+
+    void SolvePart1(string[] input)
+    {
+        Part1Solution = GetPart1Solution(input);
+    }
+
+    void SolvePart2(string[] input)
+    {
+        Part2Solution = GetPart2Solution(input);
+    }
+
+    object GetPart1Solution(string[] input);
+
+    object GetPart2Solution(string[] input);
 
     // Only applicable for puzzles where part 1 and part 2 use different example input files
-    virtual bool UsePartSpecificExampleInputFiles => false;
-
-    virtual void SolvePart1(string[] input) { }
-
-    virtual void SolvePart2(string[] input) { }
+    bool UsePartSpecificExampleInputFiles => false;
 }
