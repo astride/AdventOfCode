2d7af8435e6ca85e2b4d388c8b128d5a20c79eec
Astrid Eldhuset <astrid.eldhuset@proton.me>
2023-11-29 18:31:43 +0100

    Preparing for 1. December 2023

---
+++ Source/Year2023/Day01/Solver.cs
@@ -0,0 +1,23 @@
+using Common.Interfaces;
+
+namespace Year2023;
+
+public class Day01Solver : IPuzzleSolver
+{
+	public string Title => string.Empty;
+	public object? Part1Solution { get; set; }
+	public object? Part2Solution { get; set; }
+	public object GetPart1Solution(string[] input, bool isExampleInput)
+	{
+		var result = string.Empty;
+
+		return result;
+	}
+
+	public object GetPart2Solution(string[] input, bool isExampleInput)
+	{
+		var result = string.Empty;
+
+		return result;
+	}
+}
