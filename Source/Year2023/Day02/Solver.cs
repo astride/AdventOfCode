7ecad76350da6be8bd9da97d1e0b25e831f6e236
Astrid Eldhuset <astrid.eldhuset@proton.me>
2023-12-02 09:52:00 +0100

    [ 2023 - 02 ] Part 2

--- Source/Year2023/Day02/Solver.cs
+++ Source/Year2023/Day02/Solver.cs
@@ -10,16 +10,20 @@ public class Day02Solver : IPuzzleSolver
 	public object? Part1Solution { get; set; }
 	public object? Part2Solution { get; set; }
 
-	private static readonly Regex NumberRegex = new(@"\d+");
-	private static readonly Regex WordRegex = new(@"[a-z]+");
-	private static readonly Regex RevealedCubeRegex = new(@"(\d+ [a-z]+)");
-
 	private static readonly Dictionary<string, int> MaxCubeCountByColor = new()
 	{
 		["blue"] = 14,
 		["green"] = 13,
 		["red"] = 12,
 	};
+
+	private static readonly Regex[] RevealedCubeByColorRegexes = MaxCubeCountByColor.Keys
+		.Select(key => new Regex($@"(\d+ {key})"))
+		.ToArray();
+
+	private static readonly Regex NumberRegex = new(@"\d+");
+	private static readonly Regex WordRegex = new(@"[a-z]+");
+	private static readonly Regex RevealedCubeRegex = new(@"(\d+ [a-z]+)");
 
 	public object GetPart1Solution(string[] input, bool isExampleInput)
 	{
@@ -48,7 +52,18 @@ public class Day02Solver : IPuzzleSolver
 
 	public object GetPart2Solution(string[] input, bool isExampleInput)
 	{
-		return 0;
+		var powerSum = 0;
+
+		foreach (var gameDescription in input)
+		{
+			var gameIdAndRevealedCubes = gameDescription.Split(':', StringSplitOptions.TrimEntries);
+
+			var revealedCubes = gameIdAndRevealedCubes[1];
+
+			powerSum += GetPower(revealedCubes);
+		}
+
+		return powerSum;
 	}
 
 	private static bool AnyCubeCountOvergoesMaxCount(string revealedCubes)
@@ -68,5 +83,22 @@ public class Day02Solver : IPuzzleSolver
 		}
 
 		return false;
+	}
+
+	private static int GetPower(string revealedCubes)
+	{
+		var power = 1;
+
+		foreach (var revealedCubeByColorRegex in RevealedCubeByColorRegexes)
+		{
+			var revealedCubeMatches = revealedCubeByColorRegex.Matches(revealedCubes);
+
+			var maxRevealedCubeCount = revealedCubeMatches
+				.Max(revealedCubeMatch => int.Parse(NumberRegex.Match(revealedCubeMatch.ToString()).Value));
+
+			power *= maxRevealedCubeCount;
+		}
+
+		return power;
 	}
 }
