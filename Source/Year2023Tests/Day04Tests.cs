8d5943449d90c62c2514af457ab5ff48c262d53d
Astrid Eldhuset <6726401+astride@users.noreply.github.com>
2023-12-03 11:02:08 +0100

    [ 2023 - 04 ] Preparations

---
+++ Source/Year2023Tests/Day04Tests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Year2023;
+
+namespace Year2023Tests;
+
+[TestClass]
+public class Day04Tests : TestsBase
+{
+	protected override string DirectoryName => "Day04";
+	protected override string Part1ExampleSolution => string.Empty;
+	protected override string Part1Solution => string.Empty;
+	protected override string Part2ExampleSolution => string.Empty;
+	protected override string Part2Solution => string.Empty;
+
+	public Day04Tests() => PuzzleSolver = new Day04Solver();
+    
+	[TestMethod]
+	public void Day04IsValid() => HasCorrectSolutions();
+
+	[TestMethod]
+	public void Day04IsValid_WithExampleInput() => HasCorrectSolutionsWithExampleInput();
+}
