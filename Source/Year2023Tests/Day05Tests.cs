72c85529f934b7df4fe95128f5b2616fa374e2b8
Astrid Eldhuset <astrid.eldhuset@proton.me>
2023-12-04 17:14:28 +0100

    [ 2023 - 05 ] Preparations

--- Source/Year2023Tests/Day05Tests.cs
+++ Source/Year2023Tests/Day05Tests.cs
@@ -4,19 +4,19 @@ using Year2023;
 namespace Year2023Tests;
 
 [TestClass]
-public class Day00Tests : TestsBase
+public class Day05Tests : TestsBase
 {
-	protected override string DirectoryName => "Day00";
+	protected override string DirectoryName => "Day05";
 	protected override string Part1ExampleSolution => string.Empty;
 	protected override string Part1Solution => string.Empty;
 	protected override string Part2ExampleSolution => string.Empty;
 	protected override string Part2Solution => string.Empty;
 
-	public Day00Tests() => PuzzleSolver = new Day00Solver();
+	public Day05Tests() => PuzzleSolver = new Day05Solver();
     
-	// [TestMethod]
-	public void Day00IsValid() => HasCorrectSolutions();
+	[TestMethod]
+	public void Day05IsValid() => HasCorrectSolutions();
 
-	// [TestMethod]
-	public void Day00IsValid_WithExampleInput() => HasCorrectSolutionsWithExampleInput();
+	[TestMethod]
+	public void Day05IsValid_WithExampleInput() => HasCorrectSolutionsWithExampleInput();
 }
