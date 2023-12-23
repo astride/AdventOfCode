5479bfcc7dec8dd14955414fd427bb1a6ac5932b
Astrid Eldhuset <astrid.eldhuset@proton.me>
2023-12-23 10:00:48 +0100

    [ 2023 - 21 ] Preparations

---
+++ Source/Year2023/Day21/Solver.cs
@@ -0,0 +1,23 @@
+using Common.Interfaces;
+
+namespace Year2023;
+
+public class Day21Solver : IPuzzleSolver
+{
+	public string Title => "Step Counter";
+
+	public object? Part1Solution { get; set; }
+	public object? Part2Solution { get; set; }
+
+	public object GetPart1Solution(string[] input, bool isExampleInput)
+	{
+		var steps = isExampleInput ? 6 : 64;
+		
+		return 0;
+	}
+
+	public object GetPart2Solution(string[] input, bool isExampleInput)
+	{
+		return 0;
+	}
+}
---
+++ Source/Year2023Tests/Day21Tests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Year2023;
+
+namespace Year2023Tests;
+
+[TestClass]
+public class Day21Tests : TestsBase
+{
+	protected override string DirectoryName => "Day21";
+	protected override string Part1ExampleSolution => "16"; // When using 6 steps
+	protected override string Part1Solution => string.Empty;
+	protected override string Part2ExampleSolution => string.Empty;
+	protected override string Part2Solution => string.Empty;
+
+	public Day21Tests() => PuzzleSolver = new Day21Solver();
+    
+	[TestMethod]
+	public void Day21Part1_IsValid() => VerifyPart1();
+	
+	[TestMethod]
+	public void Day21Part2_IsValid() => VerifyPart2();
+
+	[TestMethod]
+	public void Day21Part1_IsValid_WithExampleInput() => VerifyPart1WithExampleInput();
+	
+	[TestMethod]
+	public void Day21Part2_IsValid_WithExampleInput() => VerifyPart2WithExampleInput();
+}
